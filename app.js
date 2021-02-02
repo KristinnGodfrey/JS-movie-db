7c20e8ea05e801addcfc363bda66a6e5eaa1e7e8
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-02 23:28:16 +0000

    v4.1 stylelint

--- app.js
+++ app.js
@@ -12,22 +12,35 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-// app.use('', (req,res,next) => {
-//     if (res.statusCode == 404) {
-//         res.render('404', )
-//     }
-// })
-
-app.get('', (req,res) => {
+app.get('/', (req,res) => {
+    title = "Fræðslumyndbandaleigan";
     res.render('index', videos);
 });
 
 app.locals = require('./src/video-age.js');
 
-app.get('/videos/:videoId', (req,res) => {    
-    const video = videos.videos.find(video => video.id == req.params.videoId);
-    res.render('videos', {video, videos});
-})
 
+app.get('/:videoId', (req,res) => {   
+    try {
+        const video = videos.videos.find(video => video.id == req.params.videoId);
+        title = video.title
+        res.render('videos', {video, videos, title});
+    } catch (error) {
+        res.status(404);
+        res.render('error', { title: '404, síða fannst ekki' });
+    }    
+})
+function notFoundHandler(req, res, next) {
+    res.status(404);
+    res.render('error', { title: '404, síða fannst ekki' });
+  }
+  
+  function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', { title: '500, villa' });
+  }
+  
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 // listen on port 3000
 app.listen(port, () => console.info(`listening on port on localhost:${port}`));
