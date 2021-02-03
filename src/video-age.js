81aba1cf924ddc4b7046f0a059922eae580217c4
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-03 15:48:24 +0000

    v5

--- src/video-age.js
+++ src/video-age.js
@@ -16,27 +16,25 @@ const videoAgeMap = (unit) => {
       now.getDate() + daysInMonth(created.getMonth(),
         created.getFullYear()) - created.getDate()
     );
-  }
-  else {
+  } else {
     day = (now.getDate() - created.getDate());
   }
 
   let hrs = null;
   if (now.getHours > created.getHours) {
     hrs = now.getHours - created.getHours;
-  }
-  else {
+  } else {
     hrs = (now.getHours + 24) - created.getHours;
   }
 
   if (year > 1) return `Fyrir ${year} árum síðan`;
-  else if (month > 1) return `Fyrir ${month} mánuðum síðan`;
-  else if (day > 7) return `Fyrir ${Math.floor(day / 7)} vikum síðan`;
-  else if (day === 7) return `Fyrir ${Math.floor(day / 7)} viku síðan`;
-  else if (day > 1) return `Fyrir ${day} dögum síðan`;
-  else if (hrs === 1) return `Fyrir ${hrs} klukkutímum síðan`;
-  else if (hrs > 1) return `Fyrir ${hrs} klukkutímum síðan`;
-  else return unit;
+  if (month > 1) return `Fyrir ${month} mánuðum síðan`;
+  if (day > 7) return `Fyrir ${Math.floor(day / 7)} vikum síðan`;
+  if (day === 7) return `Fyrir ${Math.floor(day / 7)} viku síðan`;
+  if (day > 1) return `Fyrir ${day} dögum síðan`;
+  if (hrs === 1) return `Fyrir ${hrs} klukkutímum síðan`;
+  if (hrs > 1) return `Fyrir ${hrs} klukkutímum síðan`;
+  return unit;
 };
 
 exports.videoAgeMap = videoAgeMap;
