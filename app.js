4bcc5d50ff25c47370941de4adff73971f930ba2
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-03 13:55:00 +0000

    4.2 eslint

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
 // imports
 const express = require('express');
+
+const path = require('path');
+const videos = require('./videos.json');
+
 const app = express();
-const port = 3000
-const videos = require('./videos.json')
+const port = 3000;
 
 // static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
 
 // set views
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
-    title = "Fræðslumyndbandaleigan";
-    res.render('index', videos);
+app.get('/', (req, res) => {
+  title = 'Fræðslumyndbandaleigan';
+  res.render('index', videos);
 });
 
 app.locals = require('./src/video-age.js');
 
-
-app.get('/:videoId', (req,res) => {   
-    try {
-        const video = videos.videos.find(video => video.id == req.params.videoId);
-        title = video.title
-        res.render('videos', {video, videos, title});
-    } catch (error) {
-        res.status(404);
-        res.render('error', { title: '404, síða fannst ekki' });
-    }    
-})
-function notFoundHandler(req, res, next) {
+app.get('/:videoId', (req, res) => {
+  try {
+    const video = videos.videos.find((video) => video.id == req.params.videoId);
+    title = video.title;
+    res.render('videos', { video, videos, title });
+  } catch (error) {
     res.status(404);
     res.render('error', { title: '404, síða fannst ekki' });
   }
-  
-  function errorHandler(err, req, res, next) {
-    res.status(500);
-    res.render('error', { title: '500, villa' });
-  }
-  
-  app.use(notFoundHandler);
-  app.use(errorHandler);
+});
+
+function notFoundHandler(req, res) {
+  res.status(404);
+  res.render('error', { title: '404, síða fannst ekki' });
+}
+
+function errorHandler(req, res) {
+  res.status(500);
+  res.render('error', { title: '500, villa' });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+/* eslint-disable */
 // listen on port 3000
 app.listen(port, () => console.info(`listening on port on localhost:${port}`));
+/* eslint-enable */
--- src/video-age.js
+++ src/video-age.js
@@ -1,41 +1,42 @@
-function daysInMonth (month, year) {
-    return new Date(year, month, 0).getDate();
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 const videoAgeMap = (unit) => {
-    const now = new Date();
-    const created = new Date(unit);
+  const now = new Date();
+  const created = new Date(unit);
 
-    const year = (now.getFullYear() - created.getFullYear());
+  const year = (now.getFullYear() - created.getFullYear());
 
-    let month = ((now.getMonth()+1+12) - (created.getMonth()+1));
+  const month = ((now.getMonth() + 1 + 12) - (created.getMonth() + 1));
 
-    let day = null;
-    if (now.getDate() < created.getDate()){
-        day = (now.getDate() + daysInMonth(created.getMonth(), created.getFullYear()) - created.getDate());    
-    }
-    else {
-        day = (now.getDate() - created.getDate()); 
-    }
+  let day = null;
+  if (now.getDate() < created.getDate()) {
+    day = (
+      now.getDate() + daysInMonth(created.getMonth(),
+        created.getFullYear()) - created.getDate()
+    );
+  }
+  else {
+    day = (now.getDate() - created.getDate());
+  }
 
-    let hrs = null;
-    if (now.getHours > created.getHours){
-        hrs = now.getHours - created.getHours;    
-    }
-    else {
-        hrs = (now.getHours + 24) - created.getHours;
-    }
-    
-    if (year > 1) return `Fyrir ${year} árum síðan`;    
-    else if (month > 1) return `Fyrir ${month} mánuðum síðan`;
-    else if (day > 7) return `Fyrir ${Math.floor(day / 7)} vikum síðan`;
-    else if (day == 7) return `Fyrir ${Math.floor(day / 7)} viku síðan`;
-    else if (day > 1) return `Fyrir ${day} dögum síðan`;
-    else if (hrs == 1) return `Fyrir ${hrs} klukkutímum síðan`;
-    else if (hrs > 1) return `Fyrir ${hrs} klukkutímum síðan`;
+  let hrs = null;
+  if (now.getHours > created.getHours) {
+    hrs = now.getHours - created.getHours;
+  }
+  else {
+    hrs = (now.getHours + 24) - created.getHours;
+  }
 
-    return unit;
+  if (year > 1) return `Fyrir ${year} árum síðan`;
+  else if (month > 1) return `Fyrir ${month} mánuðum síðan`;
+  else if (day > 7) return `Fyrir ${Math.floor(day / 7)} vikum síðan`;
+  else if (day === 7) return `Fyrir ${Math.floor(day / 7)} viku síðan`;
+  else if (day > 1) return `Fyrir ${day} dögum síðan`;
+  else if (hrs === 1) return `Fyrir ${hrs} klukkutímum síðan`;
+  else if (hrs > 1) return `Fyrir ${hrs} klukkutímum síðan`;
+  else return unit;
 };
 
 exports.videoAgeMap = videoAgeMap;
-
